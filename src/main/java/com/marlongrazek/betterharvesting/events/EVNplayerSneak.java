eb11ba4bce881858e85ac01215c4b362411bc739
Marlon Grazek <business.marlongrazek@gmail.com>
2022-01-01 19:03:36 +0100

    tree growing progress

--- src/main/java/com/marlongrazek/betterharvesting/events/EVNplayerSneak.java
+++ src/main/java/com/marlongrazek/betterharvesting/events/EVNplayerSneak.java
@@ -1,9 +1,9 @@
 package com.marlongrazek.betterharvesting.events;
 
 import com.marlongrazek.betterharvesting.main.Main;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Particle;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Ageable;
 import org.bukkit.block.data.type.Sapling;
 import org.bukkit.entity.Player;
@@ -11,6 +11,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class EVNplayerSneak implements Listener {
@@ -60,7 +62,16 @@ public class EVNplayerSneak implements Listener {
                             if (sapling.getStage() != sapling.getMaximumStage()) {
                                 sapling.setStage(sapling.getStage() + 1);
                                 cropLocation.getBlock().setBlockData(sapling);
-                            } else player.sendMessage("Tree Growing soon");
+                            } else {
+
+                                boolean megaTree = fourSaplingLocations(cropLocation.getBlock()) != null;
+                                if (megaTree)
+                                    fourSaplingLocations(cropLocation.getBlock()).forEach(block -> block.setType(Material.AIR));
+                                cropLocation.getBlock().setType(Material.AIR);
+                                generateTree(cropLocation, sapling, megaTree);
+                                player.sendMessage("Tree Growing soon");
+                            }
+
                             player.spawnParticle(Particle.VILLAGER_HAPPY, cropLocation.getX() + 0.5,
                                     cropLocation.getY(), cropLocation.getZ() + 0.5, 5, 0.25, 0, 0.25);
                         }
@@ -70,5 +81,89 @@ public class EVNplayerSneak implements Listener {
         } else {
             Bukkit.getScheduler().cancelTask(taskid);
         }
+    }
+
+    public void generateTree(Location location, Sapling sapling, boolean megaTree) {
+
+        TreeType type = TreeType.TREE;
+        Random random = new Random();
+        boolean tallTree = random.nextInt(10) == 1;
+
+        // 4 sapling tree
+        if (megaTree) {
+            switch (sapling.getMaterial()) {
+                case JUNGLE_SAPLING -> type = TreeType.JUNGLE;
+                case SPRUCE_SAPLING -> type = TreeType.MEGA_REDWOOD;
+            }
+        }
+
+        // regular tree
+        else {
+            switch (sapling.getMaterial()) {
+                case ACACIA_SAPLING -> type = TreeType.ACACIA;
+                case AZALEA -> type = TreeType.AZALEA;
+                case BIRCH_SAPLING -> {
+                    if (tallTree) type = TreeType.TALL_BIRCH;
+                    else type = TreeType.BIRCH;
+                }
+                case DARK_OAK_SAPLING -> type = TreeType.DARK_OAK;
+                case JUNGLE_SAPLING -> type = TreeType.SMALL_JUNGLE;
+                case OAK_SAPLING -> {
+                    if (tallTree) type = TreeType.BIG_TREE;
+                    else type = TreeType.TREE;
+                }
+                case SPRUCE_SAPLING -> {
+                    if (tallTree) type = TreeType.TALL_REDWOOD;
+                    else type = TreeType.REDWOOD;
+                }
+            }
+        }
+
+        location.getWorld().generateTree(location, type);
+    }
+
+    public List<Block> fourSaplingLocations(Block block) {
+
+        List<Block> saplingLocations = new ArrayList<>();
+
+        Block blockNorth = block.getRelative(BlockFace.NORTH);
+        Block blockSouth = block.getRelative(BlockFace.SOUTH);
+        Block blockEast = block.getRelative(BlockFace.EAST);
+        Block blockWest = block.getRelative(BlockFace.WEST);
+
+        // northeast
+        if (blockNorth.getType() == block.getType() && blockEast.getType() == block.getType() &&
+                block.getRelative(BlockFace.NORTH_EAST).getType() == block.getType()) {
+            saplingLocations.add(blockNorth);
+            saplingLocations.add(blockEast);
+            saplingLocations.add(block.getRelative(BlockFace.NORTH_EAST));
+        }
+
+        // northwest
+        else if(blockNorth.getType() == block.getType() && blockWest.getType() == block.getType() &&
+                block.getRelative(BlockFace.NORTH_WEST).getType() == block.getType()) {
+            saplingLocations.add(blockNorth);
+            saplingLocations.add(blockWest);
+            saplingLocations.add(block.getRelative(BlockFace.NORTH_WEST));
+        }
+
+        // southeast
+        else if(blockSouth.getType() == block.getType() && blockEast.getType() == block.getType() &&
+                block.getRelative(BlockFace.SOUTH_EAST).getType() == block.getType()) {
+            saplingLocations.add(blockSouth);
+            saplingLocations.add(blockEast);
+            saplingLocations.add(block.getRelative(BlockFace.SOUTH_EAST));
+        }
+
+        // southwest
+        else if(blockSouth.getType() == block.getType() && blockWest.getType() == block.getType() &&
+                block.getRelative(BlockFace.SOUTH_WEST).getType() == block.getType()) {
+            saplingLocations.add(blockSouth);
+            saplingLocations.add(blockWest);
+            saplingLocations.add(block.getRelative(BlockFace.SOUTH_WEST));
+        }
+
+        if(saplingLocations.isEmpty()) return null;
+        return saplingLocations;
     }
 }
